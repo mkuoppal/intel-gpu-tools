e63a0681d95a7288faff19a624548f96dbc73341
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-06-17 15:43:24 +0200

    tests/gem_close_race: Use drm_open_driver helper
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>

--- tests/gem_close_race.c
+++ tests/gem_close_race.c
@@ -214,10 +214,8 @@ igt_main
 	igt_fixture {
 		int fd;
 
-		sprintf(device, "/dev/dri/card%d", drm_get_card());
-		fd = open(device, O_RDWR);
+		fd = drm_open_driver(DRIVER_INTEL);
 
-		igt_assert(fd != -1);
 		devid = intel_get_drm_devid(fd);
 		has_64bit_relocations = intel_gen(devid) >= 8;
 
