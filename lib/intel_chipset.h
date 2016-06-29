6d5f11565ad5de4ae068e2f9bd725e16299935fd
Chris Wilson <chris@chris-wilson.co.uk>
2016-06-29 11:06:09 +0100

    intel_chipset: Convert IS_BROADWELL to device info
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- lib/intel_chipset.h
+++ lib/intel_chipset.h
@@ -376,15 +376,6 @@ void intel_check_pch(void);
 				 IS_HSW_GT2(devid) || \
 				 IS_HSW_GT3(devid))
 
-#define IS_BROADWELL(devid)	((((devid) & 0xff00) != 0x1600) ? 0 : \
-				((((devid) & 0x00f0) >> 4) > 3) ? 0 : \
-				 (((devid) & 0x000f) == BDW_SPARE) ? 1 : \
-				 (((devid) & 0x000f) == BDW_ULT) ? 1 : \
-				 (((devid) & 0x000f) == BDW_HALO) ? 1 : \
-				 (((devid) & 0x000f) == BDW_SERVER) ? 1 : \
-				 (((devid) & 0x000f) == BDW_WORKSTATION) ? 1 : \
-				 (((devid) & 0x000f) == BDW_ULX) ? 1 : 0)
-
 #define IS_SKL_GT1(devid)	((devid) == PCI_CHIP_SKYLAKE_ULT_GT1	|| \
 				 (devid) == PCI_CHIP_SKYLAKE_ULX_GT1	|| \
 				 (devid) == PCI_CHIP_SKYLAKE_DT_GT1	|| \
@@ -432,6 +423,7 @@ void intel_check_pch(void);
 
 #define IS_KBL_GT4(devid)	((devid) == PCI_CHIP_KABYLAKE_HALO_GT4)
 
+#define IS_BROADWELL(devid)	(intel_device_info(devid)->is_broadwell)
 #define IS_CHERRYVIEW(devid)	(intel_device_info(devid)->is_cherryview)
 #define IS_KABYLAKE(devid)	(intel_device_info(devid)->is_kabylake)
 #define IS_SKYLAKE(devid)	(intel_device_info(devid)->is_skylake)
