dc99604fdca78f324af086cce29c1bd25a2addf2
Robert Foss <robert.foss@collabora.com>
2016-07-27 13:17:43 -0400

    demos/Makefile: Replace automake specific name of listing in Makfile.sources
    Replace the automake specific name of listings in Makefile.sources
    with something not automake specific.
    
    Signed-off-by: Robert Foss <robert.foss@collabora.com>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- demos/Android.mk
+++ demos/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES :=
+LOCAL_SRC_FILES := $(demos_prog_lists)
 
 ifeq ($(HAVE_LIBDRM_INTEL),true)
     LOCAL_SRC_FILES += $(LIBDRM_INTEL_BIN)
