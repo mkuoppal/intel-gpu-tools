mkuoppal/intel-gpu-tools

commit 0e29ce3265b40ae965cd14dfa4765a21ff50d5b7

igt_kms: Remove pan members from igt_plane, v2.
They're duplicates with src_x/y, so just use those.

Changes since v1:
- Fix order of parameters in calls to igt_fb_set_position.

Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
Reviewed-by: Ander Conselvan de Oliveira <conselvan2@gmail.com>

lib/igt_kms.c
@@ -1915,7 +1915,7 @@ static int igt_primary_plane_commit_legacy(igt_plane_t *primary,
 	igt_assert(!primary->rotation_changed);
 
 	if (!primary->fb_changed && !primary->position_changed &&
-		!primary->size_changed && !primary->panning_changed)
+		!primary->size_changed)
 		return 0;
 
 	crtc_id = pipe->crtc_id;
@@ -1927,18 +1927,18 @@ static int igt_primary_plane_commit_legacy(igt_plane_t *primary,
 
 	if (fb_id) {
 		LOG(display,
-		    "%s: SetCrtc pipe %s, fb %u, panning (%d, %d), "
+		    "%s: SetCrtc pipe %s, fb %u, src (%d, %d), "
 		    "mode %dx%d\n",
 		    igt_output_name(output),
 		    kmstest_pipe_name(pipe->pipe),
 		    fb_id,
-		    primary->pan_x, primary->pan_y,
+		    primary->src_x, primary->src_x,
 		    mode->hdisplay, mode->vdisplay);
 
 		ret = drmModeSetCrtc(display->drm_fd,
 				     crtc_id,
 				     fb_id,
-				     primary->pan_x, primary->pan_y,
+				     primary->src_x, primary->src_x,
 				     &output->id,
 				     1,
 				     mode);
@@ -1962,7 +1962,6 @@ static int igt_primary_plane_commit_legacy(igt_plane_t *primary,
 	primary->fb_changed = false;
 	primary->position_changed = false;
 	primary->size_changed = false;
-	primary->panning_changed = false;
 
 	return 0;
 }
@@ -2406,21 +2405,6 @@ void igt_fb_set_size(struct igt_fb *fb, igt_plane_t *plane,
 	plane->src_h = h;
 
 	plane->fb_changed = true;
-}
-
-void igt_plane_set_panning(igt_plane_t *plane, int x, int y)
-{
-	igt_pipe_t *pipe = plane->pipe;
-	igt_display_t *display = pipe->display;
-
-	LOG(display, "%s.%d: plane_set_panning(%d,%d)\n",
-	    kmstest_pipe_name(pipe->pipe),
-	    plane->index, x, y);
-
-	plane->pan_x = x;
-	plane->pan_y = y;
-
-	plane->panning_changed = true;
 }
 
 static const char *rotation_name(igt_rotation_t rotation)

lib/igt_kms.h
@@ -220,7 +220,6 @@ typedef struct {
 	/* state tracking */
 	unsigned int fb_changed       : 1;
 	unsigned int position_changed : 1;
-	unsigned int panning_changed  : 1;
 	unsigned int rotation_changed : 1;
 	unsigned int size_changed     : 1;
 	/*
@@ -244,8 +243,6 @@ typedef struct {
 	uint32_t src_w;
 	uint32_t src_h;
 
-	/* panning offset within the fb */
-	unsigned int pan_x, pan_y;
 	igt_rotation_t rotation;
 	uint32_t atomic_props_plane[IGT_NUM_PLANE_PROPS];
 } igt_plane_t;
@@ -326,7 +323,6 @@ void igt_pipe_set_gamma_lut(igt_pipe_t *pipe, void *ptr, size_t length);
 void igt_plane_set_fb(igt_plane_t *plane, struct igt_fb *fb);
 void igt_plane_set_position(igt_plane_t *plane, int x, int y);
 void igt_plane_set_size(igt_plane_t *plane, int w, int h);
-void igt_plane_set_panning(igt_plane_t *plane, int x, int y);
 void igt_plane_set_rotation(igt_plane_t *plane, igt_rotation_t rotation);
 void igt_crtc_set_background(igt_pipe_t *pipe, uint64_t background);
 void igt_fb_set_position(struct igt_fb *fb, igt_plane_t *plane,

tests/kms_panel_fitting.c
@@ -89,7 +89,7 @@ static void prepare_crtc(data_t *data, igt_output_t *output, enum pipe pipe,
 		ret = drmModeSetCrtc(data->drm_fd,
 				plane->pipe->crtc_id,
 				data->fb_id1,
-				plane->pan_x, plane->pan_y,
+				plane->src_x, plane->src_y,
 				&output->id,
 				1,
 				mode);

tests/kms_plane.c
@@ -321,11 +321,9 @@ test_plane_panning_with_output(data_t *data,
 	igt_plane_set_fb(primary, &primary_fb);
 
 	if (flags & TEST_PANNING_TOP_LEFT)
-		igt_plane_set_panning(primary, 0, 0);
+		igt_fb_set_position(&primary_fb, primary, 0, 0);
 	else
-		igt_plane_set_panning(primary, mode->hdisplay, mode->vdisplay);
-
-	igt_plane_set_position(primary, 0, 0);
+		igt_fb_set_position(&primary_fb, primary, mode->hdisplay, mode->vdisplay);
 
 	igt_display_commit(&data->display);
 
@@ -343,7 +341,7 @@ test_plane_panning_with_output(data_t *data,
 
 	/* reset states to neutral values, assumed by other tests */
 	igt_output_set_pipe(output, PIPE_ANY);
-	igt_plane_set_panning(primary, 0, 0);
+	igt_fb_set_position(&primary_fb, primary, 0, 0);
 
 	test_fini(data);
 }

tests/kms_plane_scaling.c
@@ -98,7 +98,7 @@ static void prepare_crtc(data_t *data, igt_output_t *output, enum pipe pipe,
 		ret = drmModeSetCrtc(data->drm_fd,
 				plane->pipe->crtc_id,
 				data->fb_id1,
-				plane->pan_x, plane->pan_y,
+				plane->src_x, plane->src_y,
 				&output->id,
 				1,
 				mode);

commit db636a7a392aa657de89bdc7ea9a15f56596751c

igt_kms: Use proper panning coordinates for legacy primary plane updates
A copy/paste error resulted in us using src_x for both the x and y
panning coordinates; make sure we use src_y instead for the appropriate
parameter.

Fixes: 0e29ce3265b ("igt_kms: Remove pan members from igt_plane, v2.")
Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
Reviewed-by: Bob Paauwe <bob.j.paauwe@intel.com>

lib/igt_kms.c
@@ -1937,13 +1937,13 @@ static int igt_primary_plane_commit_legacy(igt_plane_t *primary,
 		    igt_output_name(output),
 		    kmstest_pipe_name(pipe->pipe),
 		    fb_id,
-		    primary->src_x, primary->src_x,
+		    primary->src_x, primary->src_y,
 		    mode->hdisplay, mode->vdisplay);
 
 		ret = drmModeSetCrtc(display->drm_fd,
 				     crtc_id,
 				     fb_id,
-				     primary->src_x, primary->src_x,
+				     primary->src_x, primary->src_y,
 				     &output->id,
 				     1,
 				     mode);