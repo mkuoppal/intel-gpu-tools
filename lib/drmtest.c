mkuoppal/intel-gpu-tools

commit 9921aff583acfcde3b79c133faa0644804d67ebc

lib/drmtest: Take DRIVER_ANY into account when opening the DRM device
__drm_open_driver was changed to bail out on VGEM devices unless they
are explicitly asked for (DRIVER_VGEM). But with that change we lost
support for DRIVER_ANY.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Fixes: 7ce63894854d ("lib: Support opening vGEM device")
Cc: Chris Wilson <chris@chris-wilson.co.uk>

lib/drmtest.c
@@ -269,6 +269,10 @@ int __drm_open_driver(int chipset)
 		    is_virtio_device(fd))
 			return fd;
 
+		/* Only VGEM-specific tests should be run on VGEM */
+		if (chipset & DRIVER_ANY && !is_vgem_device(fd))
+			return fd;
+
 		close(fd);
 	}
 

commit a8d774bc9b1a95454b287a011b04be09124686af

lib/drmtest: Test for equivalence with DRIVER_ANY
Cc: Chris Wilson <chris@chris-wilson.co.uk>
Fixes: 9921aff583ac ("lib/drmtest: Take DRIVER_ANY into account when opening the DRM device")
Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

lib/drmtest.c
@@ -270,7 +270,7 @@ int __drm_open_driver(int chipset)
 			return fd;
 
 		/* Only VGEM-specific tests should be run on VGEM */
-		if (chipset & DRIVER_ANY && !is_vgem_device(fd))
+		if (chipset == DRIVER_ANY && !is_vgem_device(fd))
 			return fd;
 
 		close(fd);