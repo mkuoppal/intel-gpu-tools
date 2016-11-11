mkuoppal/intel-gpu-tools

commit 050c00d53f391aa41931229ffe230e4872a5cb4f

lib: Pass I915_TILING_Y to the kernel if Yf or Ys
GEM_SET_TILING doesn't care about Yf or Ys, so just pass Y.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

lib/ioctl_wrappers.c
@@ -158,6 +158,9 @@ int __gem_set_tiling(int fd, uint32_t handle, uint32_t tiling, uint32_t stride)
 
 	igt_require_intel(fd);
 
+	if (tiling == I915_TILING_Yf || tiling == I915_TILING_Ys)
+		tiling = I915_TILING_Y;
+
 	memset(&st, 0, sizeof(st));
 	do {
 		st.handle = handle;

commit 1af5605a35a82932c13711675bfa5f034f7920fa

lib: Pass I915_TILING_NONE if Yf or Ys
The kernel expects that BOs for framebuffers with
I915_FORMAT_MOD_Yf_TILED will have I915_TILING_NONE.

Fixes: 050c00d53f39 ("lib: Pass I915_TILING_Y to the kernel if Yf or Ys")
Cc: Tvrtko Ursulin <tvrtko.ursulin@linux.intel.com>
Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

lib/ioctl_wrappers.c
@@ -158,8 +158,9 @@ int __gem_set_tiling(int fd, uint32_t handle, uint32_t tiling, uint32_t stride)
 
 	igt_require_intel(fd);
 
+	/* The kernel doesn't know about these tiling modes, expects NONE */
 	if (tiling == I915_TILING_Yf || tiling == I915_TILING_Ys)
-		tiling = I915_TILING_Y;
+		tiling = I915_TILING_NONE;
 
 	memset(&st, 0, sizeof(st));
 	do {